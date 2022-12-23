86cdaaa08f7c5ae0e32484f8855449ddea9d7525
Arian Hosseini <arianhosseini@outlook.com>
2022-12-23 16:50:08 +0330

    created migration file / deployment file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,25 +7,17 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+	const MyContract = await hre.ethers.getContractFactory("MyContract");
+	const myContract = await MyContract.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+	await myContract.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+	console.log(`MyContract deployed to ${myContract.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
