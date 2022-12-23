32e3708030034bf7f9f33268eda7a1c9356a72fb
Arian Hosseini <arianhosseini@outlook.com>
2022-12-23 16:42:57 +0330

    connected to the local hardhat node

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,8 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+	solidity: "0.8.17",
+	networks: {
+		localhost: {},
+	},
 };
